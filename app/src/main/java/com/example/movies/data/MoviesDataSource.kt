21ded5ca944c172ab7f45a66b07028b284223f51
NeProgramist <zasko180@gmail.com>
2020-07-07 15:32:51 +0300

    ci(data): add interface of data source [./data/MoviesDataSource.kt]

---
+++ app/src/main/java/com/example/movies/data/MoviesDataSource.kt
@@ -0,0 +1,8 @@
+package com.example.movies.data
+
+import com.example.movies.domain.model.Movie
+
+interface MoviesDataSource {
+    fun getMovies(): List<Movie>
+    fun getMovie(id: Int): Movie
+}
